3556858db1258d74015e0569701bd082746a42ce
ashikkhulal <askhulal@gmail.com>
2021-07-12 05:53:23 -0500

    script 42 added

---
+++ bash_scripts_examples/script_42.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script to add users in a batch
+# fed by a text file with 1 user
+# per line (extension of script 32)
+# users are created with a default password of 'linux'
+
+# User name file
+USERFILE=$1
+
+if [ "$USERFILE" = "" ]
+  then
+    echo "Please specify an input file!"
+    exit 10
+elif test -e $USERFILE
+  then
+    for user in `cat $USERFILE`
+    do
+        echo "Creating the "$user" user..."
+                useradd -m $user ; echo "$user:linux" | chpasswd
+    done
+    exit 20
+else
+  echo "Invalid input file specified!"
+  exit 30
+fi
